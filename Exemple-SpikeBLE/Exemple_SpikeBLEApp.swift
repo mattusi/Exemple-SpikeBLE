34f8281f3d7aaed1165e1559fa8b428161eaed16
Matheus Tusi <matheus.tusi@itlean.com.br>
2021-04-05 15:28:23 -0300

    Initial Commit

---
+++ Exemple-SpikeBLE/Exemple_SpikeBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exemple_SpikeBLEApp.swift
+//  Exemple-SpikeBLE
+//
+//  Created by Matheus Tusi on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Exemple_SpikeBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
